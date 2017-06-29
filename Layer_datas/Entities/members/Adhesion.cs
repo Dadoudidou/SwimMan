ae43b10cb79f890d04686b6febb694d99662b7c7
Dadoudidou <davidviolet18@gmail.com>
2017-06-29 14:16:50 +0200

    Adhesions

---
+++ Layer_datas/Entities/members/Adhesion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Layer_datas.Entities.members
+{
+    [Table("members_adhesions")]
+    public class Adhesion
+    {
+        [Key, Column(Order = 0)]
+        public int id { get; set; }
+        [Key, Column(Order = 1)]
+        public int order_id { get; set; }
+        [Key, Column(Order = 2)]
+        public int member_id { get; set; }
+        public int section_id { get; set; }
+        public bool validate { get; set; }
+
+        [ForeignKey("order_id")]
+        public virtual orders.Order order { get; set; }
+        [ForeignKey("member_id")]
+        public virtual Member member { get; set; }
+    }
+}
